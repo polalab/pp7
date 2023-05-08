6ab18c6d7e81487d816c1487f85e2e72edbf1361
katrinarimsa <125032516+katrinarimsa@users.noreply.github.com>
2023-05-08 08:14:34 +0200

    pp7 hw

--- insert_sort.py
+++ insert_sort.py
@@ -1,1 +1,14 @@
-# TODO
+def insert_sort(arr):
+    n = len(arr) #length of the array is stored in n
+    for i in range(n): #loop that will iterate n times
+        key = arr[i] # value "key" will be inserted into the sorted portion of the array
+        j = i-1 #this variable will be used when looking for the correct position to insert key
+        while j >= 0 and key < arr[j]: #as long as j is greater or equal to 0 and key is less than j
+            arr[j + 1] = arr[j] #j element gets moved to j+1 position (all elements are shifting 1 position to the right)
+            j = j - 1 #by subtracting 1, we iterate backwards through the sorted array, looking for the correct position for key
+        arr[j + 1] = key #key is inserted into the correct position in the sorted array
+    return arr
+#corner cases - algorithm behaves differently than expected
+# array = []
+# array = [3]
+# array = [3, 3, 2, 1]
--- test_sorting.py
+++ test_sorting.py
@@ -1,5 +1,5 @@
 from bubble_sort import bubble_sort
-import insert_sort
+from insert_sort import insert_sort
 
 class TestBubble:
 
@@ -16,10 +16,18 @@ class TestBubble:
     def test_bubble_sort_3(self):
         # remember about nice name
         case = [4, 453, 2345, 2345, 32]
-        assert [4] == bubble_sort(case)
+        assert sorted(case) == bubble_sort(case)
 
-class SortInsert:
+class TestSortInsert:
 
     def test_insert_sort_1(self):
-        pass
-    # TODO
+        case = [6666, 6989, 6074, 6969, 6996, 6699, 6669]
+        assert sorted(case) == insert_sort(case)
+
+    def test_insert_sort_2(self):
+        case = []
+        assert sorted(case) == insert_sort(case)
+
+    def test_insert_sort_3(self):
+        case = [3, 3, 2, 1]
+        assert sorted(case) == insert_sort(case)
